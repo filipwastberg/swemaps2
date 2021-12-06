1b74800abbbfc8563c46d31a3e8a757e6fb20bc1
Filip Wastberg <filip.wastberg@solita.fi>
2021-12-06 15:54:34 +0100

    added globals to please RMD C

--- R/load_so.R
+++ R/load_so.R
@@ -1,6 +1,6 @@
-#' Load DeSO polgyons 
-#' 
-#' Function to read Swedish Demografiska statistikomraden (DeSO) 
+#' Load DeSO polgyons
+#'
+#' Function to read Swedish Demografiska statistikomraden (DeSO)
 #' @importFrom stringr str_extract str_remove
 #' @importFrom janitor clean_names
 #' @importFrom sf st_read
@@ -11,32 +11,32 @@
 #' @name %>%
 #' @rdname pipe
 #' @export
-#' 
+#'
 load_deso <- function(){
-  
+
   temp <- tempdir()
   url <- "https://www.scb.se/contentassets/923c3627a8a042a5b9215e8ff3bde0a3/deso_2018_2021-10-21.zip"
-  
+
   zip_name <- stringr::str_extract(url, "([^/]+.zip)")
-  
+
   download.file(url, destfile = glue::glue("{temp}/{zip_name}"), quiet = FALSE)
-  
+
   file_name <- stringr::str_remove(zip_name, ".zip")
-  
+
   unzip(glue::glue("{temp}/{zip_name}"),
         exdir = glue::glue("{temp}/{file_name}"))
-  
+
 
   file <- list.files(glue::glue("{temp}/{file_name}"), full.names = TRUE)
-  
-  sf::st_read(file) %>% 
-    dplyr::rename(geometry = geom)
-  
+
+  sf::st_read(file) %>%
+    dplyr::rename(geometry =geom)
+
 }
-#' Load RegSO polygons 
-#' 
-#' Function to read Swedish regionala statistikomrden (RegSO) 
-#' 
+#' Load RegSO polygons
+#'
+#' Function to read Swedish regionala statistikomrden (RegSO)
+#'
 #' @importFrom stringr str_extract str_remove
 #' @importFrom janitor clean_names
 #' @importFrom sf st_read
@@ -48,20 +48,20 @@ load_deso <- function(){
 #' @rdname pipe
 #' @export
 load_regso <- function(){
-  
+
   temp <- tempdir()
   url <- "https://www.scb.se/contentassets/e3b2f06da62046ba93ff58af1b845c7e/regso_2018_v1_20211103.zip"
-  
+
   zip_name <- stringr::str_extract(url, "([^/]+.zip)")
-  
+
   try_download <- download.file(url, destfile = glue::glue("{temp}/{zip_name}"))
-  
+
   file_name <- stringr::str_remove(zip_name, ".zip")
-  
+
   unzip(glue::glue("{temp}/{zip_name}"), exdir = glue::glue("{temp}/{file_name}"))
-  
+
   file <- list.files(glue::glue("{temp}/{file_name}"), full.names = TRUE)
-  
-  sf::st_read(file) %>% 
+
+  sf::st_read(file) %>%
     dplyr::rename(geometry = geom)
 }
